57b8ce4f5dc95948aebca9805432d35931d8020d
pallavipatel79 <pallavipatel7974@gmail.com>
2026-01-20 19:00:50 +0000

    new file

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+//null => 0
+
+let isLoggedIn = ""
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+// "pallavi" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
