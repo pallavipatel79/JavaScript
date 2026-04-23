0007594d523babe0bbc9df1a64a6d8cfb74c8bc5
pallavipatel79 <pallavipatel7974@gmail.com>
2026-04-23 06:42:54 +0000

    nums and dates

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -8,7 +8,7 @@ console.log(balance.toString().length);
 console.log(balance.toFixed(2));
 
 const otherNumber = 123.8966
-console.log(otherNumber.toPrecision);
+console.log(otherNumber.toPrecision());
 
 const hundreds = 100000
 console.log(hundreds.toLocaleString('en-IN'));
@@ -29,4 +29,4 @@ console.log(Math.floor(Math.random()*10) + 1);
 const min = 10
 const max = 20
 
-console.log(Math.floor(Maths.random() * (max - min + 1)) + min );
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
