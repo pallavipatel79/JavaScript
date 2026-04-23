6a1031f36a951b58b1b14db20cb46f9ff1757dce
pallavipatel79 <pallavipatel7974@gmail.com>
2026-04-23 06:56:10 +0000

    date and time updated

--- 01_basics/07_datesin.js
+++ 01_basics/07_datesin.js
@@ -3,13 +3,13 @@ let myDate = new Date();
 console.log(myDate);
 console.log(myDate.toString());
 console.log(myDate.toDateString());
-console.log(myDate.toLocaleString);
+console.log(myDate.toLocaleString());
 console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023,0,23)
+let myCreatedDate = new Date(2023,0,23)
 // let myCreatedDate = new Date(2023,0,23,5,3)
-let myCreatedDate = new Date("2023-01-14")
-// console.log(myCreatedDate.toDateString());
+// let myCreatedDate = new Date("2023-01-14")
+console.log(myCreatedDate.toDateString());
 
 let myTimeStamp = Date.now()
 console.log(myTimeStamp);//millisecond 
@@ -19,7 +19,7 @@ console.log(Math.floor(Date.now()/1000));//convert to sec
 let newDate = new Date();
 console.log(newDate);
 console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay);
+console.log(newDate.getDay());
 
 // `${newDate.getDay()}` and time
 
