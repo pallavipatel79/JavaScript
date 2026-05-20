00fd2dd8453343a090b3e92db58710fd8007a8b0
pallavipatel79 <pallavipatel7974@gmail.com>
2026-05-20 13:51:04 +0000

    for in,forEach

---
+++ 05_iterations/five.js
@@ -0,0 +1,5 @@
+const coding = ["js","cpp","python","ruby","swift"]
+
+coding.foreach( function (val){
+    log
+})
---
+++ 05_iterations/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: 'Javascript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swift by Apple',
+}
+
+// for(const key in myObject){
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+//node 05_iterations/four.js
+
+const programming = ["js","cpp","py","rb","swift"]
+// for(const key in programming){
+//     console.log(programming[key]);
+    
+// }
+
+const map = new Map()//not iteratable
+map.set('IN', 'India')
+map.set('US', 'United States')
+map.set('Fr', 'France')
+map.set('IN', 'India')
+
+// for(const key in map){
+//     console.log(key);  
+// }
