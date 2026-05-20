fbde5166ac821c60c5d54784d12d218be093d880
pallavipatel79 <pallavipatel7974@gmail.com>
2026-05-20 12:24:04 +0000

    for ,  while and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 0
+// while (index <= 10) {
+//     console.log(index);
+//     index++;
+// }
+
+
+let array = ["flash", "batman", "superman"]
+let i = 0
+while (i < array.length) {
+    console.log(array[i]);
+    i++;
+}
+
+
+let  score = 11
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score < 10);
